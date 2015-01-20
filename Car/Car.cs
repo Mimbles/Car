e62005473eb9dbf27102ba3da3d569331ff89ece
James Brown <james---b@hotmail.co.uk>
2015-01-20 20:02:27 +0000

    Update Car.cs
    Various changes

--- Car/Car.cs
+++ Car/Car.cs
@@ -8,19 +8,16 @@ namespace Car
 {
     class Car
     {
-        int fuelAmount;
+        int fuelAmount = 100; // We assume the tank starts with 100% of the potential fuel for simplicity
         string model;
         string colour;
         int noOfDoors;
-        int milesDriven;
 
-        public Car(string model, string colour, int noOfDoors, int miles)
+        public Car(string model, string colour, int noOfDoors)
         {
             this.model = model;
             this.colour = colour;
             this.noOfDoors = noOfDoors;
-            this.milesDriven = miles;
-            fuelAmount = 100 - (miles / 10);
         }
 
         public int Fuel
@@ -31,12 +28,28 @@ namespace Car
 
            public override string ToString()
         {
-            return "Model: " + model + "\n" + "Colour: " + colour + "\n" + "Number of doors: " + noOfDoors; 
+            return "Model: " + model + "\n" + "Colour: " + colour + "\n" + "Number of doors: " + noOfDoors + "\n" + "Amount of Fuel" + fuelAmount + "%"; 
         }
 
            public void DistanceDrive()
            {
+               Console.WriteLine("How many miles do you want to drive?");
                milesDriven = Convert.ToInt32(Console.ReadLine());
+               
+               //Check the car has sufficient fuel for the trip
+               if (Math.Ceiling(milesDriven / 10) < fuelAmount)
+               {
+                   Console.WriteLine("Insuffient fuel");
+                   return;
+               }
+               else
+               {
+                   //Driving a car uses fuel, so the total amount of fuel should be reduced
+                   //10 Miles = 1% of fuel
+                   //Always round up to the nearest %
+                   fuelAmount -= Math.Ceiling(milesDriven / 10);
+               }
+               
            }
 
         
